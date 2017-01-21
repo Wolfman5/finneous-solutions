82ef18b1cb62cea0e45e757716ee86110cb94293
Dylan Perry <dylanperry@dylans-mbp.attlocal.net>
2017-01-21 18:10:33 -0500

    Angular2 First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostsService } from './posts.service';
+import { HomeComponent } from './home/home.component';
+
+const ROUTES = [
+ {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path:'home',
+    component: HomeComponent
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  }
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    HomeComponent //PostsComponent injected here
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [PostsService],// Add the posts service
   bootstrap: [AppComponent]
 })
 export class AppModule { }
